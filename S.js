5f6c2ce7c65c6680b5139f57579dd3db74a9ea7b
blankboii <blan3bo1@gmail.com>
2025-08-06 21:31:05 -0700

    Delete S.js

--- S.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { message } = req.body;
-        console.log('Received message:', message);
-        res.status(200).json({ message: `Success! Received: "${message}"` });
-    } else {
-        res.status(405).json({ message: 'Method not allowed' });
-    }
-}
